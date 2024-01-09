c32e71d34dba13998859aa00acadf577635828c7
Mateus <mateuslannes100@gmail.com>
2024-01-09 17:33:02 -0300

    Add files via upload

---
+++ my-project1/build/6407.53bf4211.chunk.js
@@ -0,0 +1,1 @@
+"use strict";(self.webpackChunkmy_project_1=self.webpackChunkmy_project_1||[]).push([[6407],{6407:(s,e,u)=>{u.r(e),u.d(e,{default:()=>c});const c={}}}]);
---
+++ my-project1/build/7730.9d45138f.chunk.js
@@ -0,0 +1,1 @@
+(self.webpackChunkmy_project_1=self.webpackChunkmy_project_1||[]).push([[7730],{22868:()=>{},14777:()=>{},99830:()=>{},70209:()=>{},87414:()=>{}}]);
